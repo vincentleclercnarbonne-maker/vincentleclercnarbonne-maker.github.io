60bd7ce78f325e6e9c7eddb8e3e4afb6efda0e98
vincentleclercnarbonne-maker <vincent.leclerc.narbonne@gmail.com>
2026-08-03 07:59:20 +0200

    Actualiser le cache CRM v37

--- crm/sw.js
+++ crm/sw.js
@@ -1,4 +1,4 @@
-const CACHE='crm-technimat-v36';
+const CACHE='crm-technimat-v37';
 const ASSETS=['./','index.html','style.css','security.js','app.js','app-plus.js','planning.js','planning-filter.js','tour-sync.js','logo-technimat.svg','icon.svg','manifest.webmanifest'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
